a28abebe35b5033ad41fcb3f57a51865f6cc1575
Jayant Rohila <jayant@bnimile.com>
2024-03-20 15:03:16 +0530

    removed user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { AuthController } from './auth/auth.controller';
-import { UserService } from './user/user.service';
-import { AuthService } from './auth/auth.service';
-import { UserModule } from './user/user.module';
-import { EnvModule } from './env/env.module';
 import { ConfigModule } from '@nestjs/config';
-import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: '10.10.11.171',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'db',
-      autoLoadModels: true,
-      synchronize: true,
-    }),
-    EnvModule,
-    UserModule,
-    EnvModule,
-  ],
-  controllers: [AppController, UserController, AuthController],
-  providers: [AppService, UserService, AuthService],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { EnvService } from './env/env.service';
 import { config } from 'dotenv';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(EnvService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(3000);
 }
 bootstrap();
